ae44e803c32dbd204fd964ee1c3573241de9267e
nayala98 <nathaniel.ayala20@gmail.com>
2021-05-23 16:26:36 -0500

    Installing Dependencies

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,34 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// promptUser returns answers object
+const promptUser = () => {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your GitHub user name?",
+                name: "username"
+            },
+            {
+                type: "input",
+                message: "What is your email address?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is your project's name?",
+                name: "projectName"
+            },
+            {
+                type: "input",
+                message: "Please write a short description of your project.",
+                name: "description"
+            },
+        ])
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// Run the README generator
+promptUser().then((answers) => {
+    console.log(answers);
+});
