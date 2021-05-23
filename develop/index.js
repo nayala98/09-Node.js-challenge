38577d41970bcfe457e9f2b462218515a5888e9f
nayala98 <nathaniel.ayala20@gmail.com>
2021-05-23 17:01:28 -0500

    Initial Logic Started

--- develop/index.js
+++ develop/index.js
@@ -1,34 +1,97 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+const generateMd = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// promptUser returns answers object
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project.",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "Unlicense",
+            "Apache 2.0",
+            "GNU v3"
+        ]
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repository?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repository?",
+        name: "contribute"
+    },
+]
+
+// Function to promptUser returns answers object
 const promptUser = () => {
     return inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is your GitHub user name?",
-                name: "username"
-            },
-            {
-                type: "input",
-                message: "What is your email address?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "What is your project's name?",
-                name: "projectName"
-            },
-            {
-                type: "input",
-                message: "Please write a short description of your project.",
-                name: "description"
-            },
-        ])
+    .prompt(questions);
+};
+
+// Function to write README file
+function writeToFile(fileName, data) {
+    return writeFileAsync(fileName, data);
 }
 
+// Function to start program
+async function init() {
+    try {
+        // Ask user for answers to questions
+        const answers = await promptUser();
+        // Create markdown content from user answers
+        const fileContent = generateMd(answers);
+        // Write markdown content to README.md file
+        await writeToFile("./output/README.md", fileContent);
+        // Notify user that file has been written
+        console.log("README.md created");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Function call to initialize program
+init();
+
+/*
 // Run the README generator
 promptUser().then((answers) => {
     console.log(answers);
-});
+});*/
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,18 +1,45 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
+  // set url for license
+  switch (data.license) {
+    case "MIT":
+      data.licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+      break;
+    case "Unlicense":
+      data.licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+      break;
+    case "Apache 2.0":
+      data.licenseBadge = "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+      break;
+    case "GNU v3":
+      data.licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      break;
+  }
+
+  // return markdown content
   return `# ${data.title}
+${data.licenseBadge}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+This repository is licensed under the ${data.license} license.
+## Contributing
+${data.contribute}
+## Tests
+${data.tests}
+## Questions
+Questions about this repository? Please contact me at [${data.email}](mailto:${data.email}). View more of my work at [${data.username}](https://github.com/${data.username}) 
 `;
 }
 
